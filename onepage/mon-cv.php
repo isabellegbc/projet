60607462ef81b553f2beda74d9f408fc75935fa8
isabellegbc <isabelle.gbc@laposte.net>
2020-02-12 14:42:44 +0100

    update dossiers

---
+++ onepage/mon-cv.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+</head>
+<body>
+<header>
+
+
+<nav>
+
+<a href="/COMPETENCESPRO/">COMPETENCES</a> 
+
+<a href="/COMPÉTENCES TECHNIQUES/">COMPETENCES TECHNIQUES</a> 
+
+<a href="EXPERIENCES/">EXPERIENCES PROFESSIONNELLE</a> 
+
+<a href="/ETUDES/">ETUDES & fORMATIONS</a>
+
+<a href="/Langues/">LANGUES</a>
+
+<a href="/DISPONIBILITÉ/">DISPONIBILITÉ</a>
+
+</nav> 
+
+
+
+    <div style ="text-align:center">
+    <h1>MON CURRICULUM VITAE</h1>
+    </div>
+    <div>
+    class = "image"src="photo.jpg" alt="image">
+    </div>
+
+    <?php 
+        $nomCandidat = "GUEZATI";    
+        $prenomCandidat = "ISABELLE";
+        $adresseCandidat = "94 la grande bastide cazaulx";
+        $telCandidat = "0659927349";
+        $emailCandidat = "isabelle.gbc@laposte.net";
+?>
+ 
+ <style type="text/css">
+p {
+  color: #26b72b;
+}
+</style>
+ <article>
+        <p><?php echo $nomCandidat?></p>
+        <p><?php echo $prenomCandidat?></p>
+        <p><?php echo $adresseCandidat?></p>
+        <p><?php echo $telCandidaABELLE?></p>
+        $adresseCandidat = "94 la grande bastide cazaulx";
+        $telCandidat = "0659927349";
+        $emailCandidat = "isabelle.gbc@laposte.net";
+?>
+ 
+ <style type="text/css">
+p {
+  color: #26b72b;
+}t?></p> 
+        <p><?php echo $emailCandidat?></p>       
+ </article>
+ 
+ <style type="text/css">
+p {
+  color: #26b72b;
+}
+</style>
+<article>
+<p>COMPETENCES PROFESSIONNELLES</P>
+ 
+ 
+    </header>
+
+    
+       
+        <//body>
+</html>
